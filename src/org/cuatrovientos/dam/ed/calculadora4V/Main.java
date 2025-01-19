92300fab90ca4dbe5beac491cc0e3105696817ab
AsierTxokarro <asiertxokarro@gmail.com>
2025-01-19 13:29:59 +0100

    Crear nuestra primera calculadora

--- src/org/cuatrovientos/dam/ed/calculadora4V/Main.java
+++ src/org/cuatrovientos/dam/ed/calculadora4V/Main.java
@@ -1,10 +1,38 @@
 package org.cuatrovientos.dam.ed.calculadora4V;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 
+		    	
+		    	
+		        Scanner scan = new Scanner(System.in);
+		        
+		        double num1, num2 = 0;
+		        char operador;
+		        System.out.print("Escribe el primer numero: ");
+		        num1 = scan.nextDouble();
+		        System.out.print("Escribe el segundo numero: ");
+		        num2 = scan.nextDouble();
+		        System.out.print("Introduce operacion: ( +, -, *, / ): ");
+		        operador = scan.next().charAt(0);
+		        
+		        if(operador== '+') {
+		        	System.out.println(num1 + num2);
+		        }else if (operador == '-') {
+		        	System.out.println(num1 - num2);
+		        }else if(operador == '*') {
+		        	System.out.println(num1 * num2);
+		        }else if( operador == '/') {
+		        	System.out.println(num1 / num2);
+		        }
+		        	
+		        scan.close();
+		    }
+		}
 
-	}
+	
 
-}
+
