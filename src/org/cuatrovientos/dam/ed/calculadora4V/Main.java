ff2ccb971c7891d45acb11ea289df9efa8889777
AsierTxokarro <asiertxokarro@gmail.com>
2025-01-19 13:46:19 +0100

    refactorizacion y formateo

--- src/org/cuatrovientos/dam/ed/calculadora4V/Main.java
+++ src/org/cuatrovientos/dam/ed/calculadora4V/Main.java
@@ -5,34 +5,34 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		 
-		    	
-		    	
-		        Scanner scan = new Scanner(System.in);
-		        
-		        double num1, num2 = 0;
-		        char operador;
-		        System.out.print("Escribe el primer numero: ");
-		        num1 = scan.nextDouble();
-		        System.out.print("Escribe el segundo numero: ");
-		        num2 = scan.nextDouble();
-		        System.out.print("Introduce operacion: ( +, -, *, / ): ");
-		        operador = scan.next().charAt(0);
-		        
-		        if(operador== '+') {
-		        	System.out.println(num1 + num2);
-		        }else if (operador == '-') {
-		        	System.out.println(num1 - num2);
-		        }else if(operador == '*') {
-		        	System.out.println(num1 * num2);
-		        }else if( operador == '/') {
-		        	System.out.println(num1 / num2);
-		        }
-		        	
-		        scan.close();
-		    }
+
+		Scanner scan = new Scanner(System.in);
+
+		double num1, num2 = 0;
+		char operando;
+		
+		System.out.print("Escribe el primer numero: ");
+		
+		  num1 = scan.nextDouble();
+		System.out.print("Escribe el segundo numero: ");
+		
+		  num2 = scan.nextDouble();
+		System.out.print("Introduce operacion: ( +, -, *, / ): ");
+		
+		  operando = scan.next().charAt(0);
+
+		
+		if (operando == '+') {
+			System.out.println(num1 + num2);
+		} else if (operando == '-') {
+			System.out.println(num1 - num2);
+		} else if (operando == '*') {
+			System.out.println(num1 * num2);
+		} else if (operando == '/') {
+			System.out.println(num1 / num2);
 		}
 
-	
-
-
+		
+		scan.close();
+	}
+}
